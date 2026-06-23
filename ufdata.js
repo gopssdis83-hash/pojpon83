5e0d687982cf3bd189aa59ff98aafde0bdb043df
gopssdis83-hash <gops.sdis83@gmail.com>
2026-06-23 17:19:40 +0200

    Add files via upload

---
+++ ufdata.js
@@ -0,0 +1,93 @@
+// ufData.js
+
+window.ufData = {
+
+  "CIS SAINT CYR SUR MER": { jour: 8, nuit: 8, mini: { jour: 6, nuit: 6 }, cible: { jour: 9, nuit: 9 } },
+
+  "CIS BANDOL": { jour: 6, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 7, nuit: 4 } },
+  "CIS LE BEAUSSET": { jour: 6, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 7, nuit: 7 } },
+  "CIS la Cadière d'Azur": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 0, nuit: 4 } },
+  "CIS SIGNES": { jour: 3, nuit: 2, mini: { jour: 2, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CI LA SEYNE SUR MER": { jour: 4, nuit: 4, mini: { jour: 3, nuit: 3 }, cible: { jour: 7, nuit: 7 } },
+  "CIS LA SEYNE SUR MER SUD": { jour: 10, nuit: 10, mini: { jour: 9, nuit: 9 }, cible: { jour: 12, nuit: 12 } },
+  "CIS SIX FOURS LES PLAGES": { jour: 9, nuit: 7, mini: { jour: 7, nuit: 4 }, cible: { jour: 10, nuit: 7 } },
+  "CIS SANARY SUR MER": { jour: 6, nuit: 6, mini: { jour: 4, nuit: 3 }, cible: { jour: 7, nuit: 7 } },
+
+  "CIS TOULON CENTRE": { jour: 21, nuit: 18, mini: { jour: 17, nuit: 17 }, cible: { jour: 23, nuit: 21 } },
+  "CIS TOULON OUEST": { jour: 14, nuit: 14, mini: { jour: 12, nuit: 10 }, cible: { jour: 17, nuit: 17 } },
+  "CIS OLLIOULES": { jour: 4, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 7, nuit: 4 } },
+
+  "CIS HYERES": { jour: 14, nuit: 12, mini: { jour: 12, nuit: 10 }, cible: { jour: 17, nuit: 17 } },
+  "CIS LA GARDE": { jour: 12, nuit: 11, mini: { jour: 9, nuit: 9 }, cible: { jour: 14, nuit: 12 } },
+  "CIS SOLLIES PONT": { jour: 7, nuit: 6, mini: { jour: 6, nuit: 4 }, cible: { jour: 8, nuit: 7 } },
+  "CIS COLLOBRIERES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS BORMES LES MIMOSAS": { jour: 9, nuit: 8, mini: { jour: 6, nuit: 6 }, cible: { jour: 10, nuit: 9 } },
+  "CIS LA LONDE LES MAURES": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS CUERS": { jour: 4, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 7, nuit: 4 } },
+
+  "CIS PIERREFEU DU VAR": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS PUGET VILLE": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS SAINT MAXIMIN": { jour: 9, nuit: 7, mini: { jour: 6, nuit: 6 }, cible: { jour: 10, nuit: 7 } },
+  "CIS SAINT ZACHARIE": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS NANS LES PINS": { jour: 3, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS PLAN D'AUPS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS POURRIERES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS GINASSERVIS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } },
+  "CIS VINON SUR VERDON": { jour: 2, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS RIANS": { jour: 3, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS BARJOLS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS CARCES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 0 } },
+  "CIS COTIGNAC": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS BRIGNOLES": { jour: 11, nuit: 10, mini: { jour: 10, nuit: 9 }, cible: { jour: 12, nuit: 12 } },
+  "CIS GAREOULT": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS TOURVES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS LE LUC": { jour: 9, nuit: 7, mini: { jour: 6, nuit: 6 }, cible: { jour: 10, nuit: 8 } },
+
+  "CIS LES MAYONS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS GONFARON": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 5 } },
+  "CIS PIGNANS": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 8, nuit: 4 } },
+
+  "CIS CAVALAIRE SUR MER": { jour: 6, nuit: 3, mini: { jour: 4, nuit: 3 }, cible: { jour: 7, nuit: 4 } },
+  "CIS GRIMAUD": { jour: 6, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 7, nuit: 7 } },
+  "CIS SAINT TROPEZ": { jour: 7, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 7, nuit: 7 } },
+  "CIS LE RAYOL CANADEL": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS SAINTE MAXIME": { jour: 8, nuit: 6, mini: { jour: 7, nuit: 6 }, cible: { jour: 9, nuit: 8 } },
+  "CIS LA GARDE FREINET": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 0 }, cible: { jour: 7, nuit: 4 } },
+  "CIS PLAN DE LA TOUR": { jour: 2, nuit: 2, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS FREJUS": { jour: 13, nuit: 12, mini: { jour: 12, nuit: 10 }, cible: { jour: 16, nuit: 13 } },
+  "CIS SAINT RAPHAEL": { jour: 11, nuit: 8, mini: { jour: 9, nuit: 7 }, cible: { jour: 13, nuit: 9 } },
+
+  "CIS ROQUEBRUNE SUR ARGENS": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS LES ADRETS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 7, nuit: 4 } },
+
+  "CIS FAYENCE": { jour: 6, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 7, nuit: 7 } },
+  "CIS MONTAUROUX": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 7, nuit: 7 } },
+  "CIS SAINT PAUL EN FORÊT": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 7, nuit: 4 } },
+  "CIS SEILLANS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS TANNERON": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS LE MUY": { jour: 7, nuit: 6, mini: { jour: 6, nuit: 4 }, cible: { jour: 8, nuit: 7 } },
+  "CIS LES ARCS SUR ARGENS": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS VIDAUBAN": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS DRAGUIGNAN": { jour: 12, nuit: 9, mini: { jour: 9, nuit: 8 }, cible: { jour: 13, nuit: 12 } },
+  "CIS CALLAS": { jour: 3, nuit: 2, mini: { jour: 2, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS COMPS SUR ARTUBY": { jour: 3, nuit: 0, mini: { jour: 2, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS LORGUES": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS AUPS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } },
+  "CIS SALERNES": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } },
+  "CIS TOURTOUR": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS PORQUEROLLES": { jour: 4, nuit: 4, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } }
+
+};
---
+++ ufdata2.js
@@ -0,0 +1,99 @@
+// ufData2.js
+
+window.ufData2 = {
+
+  "CIS SAINT CYR SUR MER": { jour: 9, nuit: 8, mini: { jour: 7, nuit: 7 }, cible: { jour: 10, nuit: 9 } },
+
+  "CIS BANDOL": { jour: 7, nuit: 7, mini: { jour: 4, nuit: 3 }, cible: { jour: 8, nuit: 8 } },
+  "CIS LE BEAUSSET": { jour: 6, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 8, nuit: 8 } },
+  "CIS la Cadière d'Azur": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 8, nuit: 4 } },
+  "CIS SIGNES": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 0 }, cible: { jour: 8, nuit: 4 } },
+
+  "CI LA SEYNE SUR MER": { jour: 4, nuit: 4, mini: { jour: 3, nuit: 3 }, cible: { jour: 7, nuit: 7 } },
+  "CIS LA SEYNE SUR MER SUD": { jour: 12, nuit: 10, mini: { jour: 9, nuit: 9 }, cible: { jour: 14, nuit: 12 } },
+  "CIS SIX FOURS LES PLAGES": { jour: 10, nuit: 9, mini: { jour: 7, nuit: 7 }, cible: { jour: 11, nuit: 11 } },
+  "CIS SANARY SUR MER": { jour: 9, nuit: 6, mini: { jour: 6, nuit: 4 }, cible: { jour: 11, nuit: 7 } },
+
+  "CIS TOULON CENTRE": { jour: 21, nuit: 18, mini: { jour: 20, nuit: 17 }, cible: { jour: 25, nuit: 21 } },
+  "CIS TOULON OUEST": { jour: 14, nuit: 14, mini: { jour: 12, nuit: 10 }, cible: { jour: 17, nuit: 17 } },
+  "CIS OLLIOULES": { jour: 6, nuit: 6, mini: { jour: 3, nuit: 2 }, cible: { jour: 8, nuit: 7 } },
+
+  "CIS HYERES": { jour: 16, nuit: 16, mini: { jour: 14, nuit: 12 }, cible: { jour: 22, nuit: 18 } },
+  "CIS LA GARDE": { jour: 4, nuit: 12, mini: { jour: 12, nuit: 10 }, cible: { jour: 22, nuit: 18 } },
+  "CIS SOLLIES PONT": { jour: 8, nuit: 6, mini: { jour: 7, nuit: 4 }, cible: { jour: 9, nuit: 7 } },
+  "CIS SABRAN": { jour: 3, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 3, nuit: 0 } },
+
+  "CIS BORMES LES MIMOSAS": { jour: 15, nuit: 14, mini: { jour: 12, nuit: 12 }, cible: { jour: 16, nuit: 15 } },
+  "CIS LA LONDE LES MAURES": { jour: 7, nuit: 7, mini: { jour: 4, nuit: 4 }, cible: { jour: 8, nuit: 8 } },
+  "CIS CAVALIERE": { jour: 3, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 3, nuit: 0 } },
+  "CIS CUERS": { jour: 4, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 7, nuit: 4 } },
+  "CIS COLLOBRIERES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS PIERREFEU DU VAR": { jour: 4, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 8, nuit: 4 } },
+  "CIS PUGET VILLE": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS SAINT MAXIMIN": { jour: 9, nuit: 7, mini: { jour: 6, nuit: 6 }, cible: { jour: 10, nuit: 7 } },
+  "CIS SAINT ZACHARIE": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS NANS LES PINS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS PLAN D AUPS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 6, nuit: 4 } },
+  "CIS POURRIERES": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS GINASSERVIS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } },
+  "CIS VINON SUR VERDON": { jour: 3, nuit: 3, mini: { jour: 0, nuit: 0 }, cible: { jour: 7, nuit: 4 } },
+
+  "CIS RIANS": { jour: 3, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS BARJOLS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS CARCES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 0 } },
+  "CIS COTIGNAC": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS BRIGNOLES": { jour: 12, nuit: 10, mini: { jour: 11, nuit: 9 }, cible: { jour: 14, nuit: 12 } },
+  "CIS GAREOULT": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 6, nuit: 4 } },
+  "CIS TOURVES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS LE LUC": { jour: 9, nuit: 7, mini: { jour: 6, nuit: 6 }, cible: { jour: 10, nuit: 8 } },
+
+  "CIS GONFARON": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } },
+  "CIS lES MAYONS": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 6, nuit: 4 } },
+  "CIS PIGNANS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 8, nuit: 4 } },
+
+  "CIS CAVALAIRE SUR MER": { jour: 9, nuit: 7, mini: { jour: 7, nuit: 4 }, cible: { jour: 11, nuit: 8 } },
+  "CIS GRIMAUD": { jour: 12, nuit: 7, mini: { jour: 9, nuit: 6 }, cible: { jour: 14, nuit: 9 } },
+  "CIS SAINT TROPEZ": { jour: 13, nuit: 7, mini: { jour: 9, nuit: 6 }, cible: { jour: 16, nuit: 10 } },
+
+  "CIS SAINTE MAXIME": { jour: 11, nuit: 11, mini: { jour: 10, nuit: 9 }, cible: { jour: 12, nuit: 12 } },
+  "CIS LA GARDE FREINET": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 0 }, cible: { jour: 7, nuit: 4 } },
+  "CIS PLAN DE LA TOUR": { jour: 2, nuit: 2, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS LE RAYOL CANADEL": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS GASSIN": { jour: 3, nuit: 3, mini: { jour: 0, nuit: 0 }, cible: { jour: 3, nuit: 3 } },
+
+  "CIS FREJUS": { jour: 20, nuit: 15, mini: { jour: 19, nuit: 12 }, cible: { jour: 22, nuit: 17 } },
+  "CIS SAINT RAPHAEL": { jour: 14, nuit: 11, mini: { jour: 12, nuit: 9 }, cible: { jour: 17, nuit: 12 } },
+
+  "CIS ROQUEBRUNE SUR ARGENS": { jour: 7, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 8, nuit: 7 } },
+  "CIS LES ADRETS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 8, nuit: 4 } },
+
+  "CIS FAYENCE": { jour: 6, nuit: 6, mini: { jour: 4, nuit: 4 }, cible: { jour: 7, nuit: 7 } },
+  "CIS BAGNOLS EN FORET": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 0 } },
+  "CIS MONTAUROUX": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 8, nuit: 7 } },
+  "CIS SAINT PAUL": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 8, nuit: 4 } },
+  "CIS SEILLANS": { jour: 3, nuit: 3, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+  "CIS TANNERON": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 7, nuit: 4 } },
+
+  "CIS LE MUY": { jour: 9, nuit: 6, mini: { jour: 8, nuit: 4 }, cible: { jour: 10, nuit: 7 } },
+  "CIS LES ARCS SUR ARGENS": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS VIDAUBAN": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS DRAGUIGNAN": { jour: 14, nuit: 10, mini: { jour: 12, nuit: 9 }, cible: { jour: 15, nuit: 12 } },
+  "CIS CALLAS": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 2 }, cible: { jour: 4, nuit: 4 } },
+  "CIS COMPS SUR ARTUBY": { jour: 2, nuit: 0, mini: { jour: 3, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS LORGUES": { jour: 3, nuit: 3, mini: { jour: 2, nuit: 2 }, cible: { jour: 8, nuit: 4 } },
+  "CIS AUPS": { jour: 4, nuit: 4, mini: { jour: 4, nuit: 4 }, cible: { jour: 7, nuit: 7 } },
+  "CIS AIGUINES": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 3, nuit: 3 } },
+  "CIS SALERNES": { jour: 3, nuit: 3, mini: { jour: 3, nuit: 3 }, cible: { jour: 4, nuit: 4 } },
+  "CIS TOURTOUR": { jour: 0, nuit: 0, mini: { jour: 0, nuit: 0 }, cible: { jour: 4, nuit: 4 } },
+
+  "CIS PORQUEROLLES": { jour: 7, nuit: 7, mini: { jour: 4, nuit: 4 }, cible: { jour: 10, nuit: 7 } },
+  "CIS PORT CROS": { jour: 3, nuit: 7, mini: { jour: 2, nuit: 2 }, cible: { jour: 3, nuit: 3 } }
+
+};
